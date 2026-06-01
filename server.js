712c08fa0bc0e95dcb450f859e84ded7a45cd2f2
nijatechmob <nijatechmob@gmail.com>
2026-06-01 13:35:35 +0530

    update

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+// require("dotenv").config();
+
+// const app = require("./src/app");
+// const connectDB = require("./src/config/db");
+
+// connectDB();
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server Running On Port ${process.env.PORT}`);
+// });
+
 require("dotenv").config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running On Port ${process.env.PORT}`);
-});
+    app.listen(process.env.PORT, () => {
+      console.log(`Server Running On Port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+startServer();
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,32 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Trying MongoDB...");
+    console.log(process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
 
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error.message);
-    process.exit(1);
+    console.log("Mongo Error:", error.message);
+    throw error;
   }
 };
 
