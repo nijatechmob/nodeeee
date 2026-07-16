3c056a91a04f64194d46f5de45871e32fa36a319
nijatechmob <nijatechmob@gmail.com>
2026-07-16 15:25:12 +0530

    Update itemRoutes.js

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -3,7 +3,7 @@ const express=require("express");
 const router=express.Router();
 
 const auth = require("../middleware/authMiddleware");
-const adminOnly=require("../middleware/adminOnly");
+const adminOnly=require("../middleware/adminMiddleware");
 
 const{
 addItem,
